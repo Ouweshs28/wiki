f3a3b7c5bb99f4f103684cfdba27bb7ae5995048
Ouwesh Seeroo <ouweshseeroo@gmail.com>
2025-08-03 15:20:11 +0400

    Deploy website - based on ad7e7075a4cdb6e8f617f8da0085e5cdbaaca8fb

--- assets/js/acecf23e.e415cd85.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkouwesh_wiki=self.webpackChunkouwesh_wiki||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/wiki/blog","blogTitle":"Blog","authorsListPath":"/wiki/blog/authors"}')}}]);
